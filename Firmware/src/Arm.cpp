4e0f8e8e6ea0be5ca43168dceefb6514d735e495
Nuno <nm@ledgerassets.com>
2018-08-06 00:55:24 +0800

    Ported protcol from MQTT to WebSocket and made Arm handler more robust and friendly

--- Firmware/src/Arm.cpp
+++ Firmware/src/Arm.cpp
@@ -4,42 +4,91 @@ void Arm::begin() {
     servoPWM.begin();
     servoPWM.setPWMFreq(60);  // Analog servos run at ~60 Hz updates~
 
-    // Set scServo RS485 to transmit
+    delay(100);
+}
+
+void Arm::beginSCServo(HardwareSerial &scServoSerial) {
     pinMode(scServoTXEnablePin, OUTPUT);
     pinMode(scServoRXEnablePin, OUTPUT);
     digitalWrite(scServoTXEnablePin, HIGH);
     digitalWrite(scServoRXEnablePin, HIGH);
 
-    //HardwareSerial sc(2);
-    //sc.begin(115200);
-    //scServo.pSerial = &sc;
-    delay(500);
-
-    //scServo.EnableTorque(scServoID1, 1);
-    //scServo.EnableTorque(scServoID2, 1);
     delay(100);
-}
-
-void Arm::beginSCServo(HardwareSerial &scServoSerial) {
-    scServoSerial.begin(115200);
 
     scServoSerial.begin(115200);
+
     scServo.pSerial = &scServoSerial;
     delay(500);
-
-    scServo.EnableTorque(scServoID1, 1);
-    scServo.EnableTorque(scServoID2, 1);
-    delay(100);
 }
 
-void Arm::setAnalogServoPWMFreq(float freq) {
-    servoPWM.setPWMFreq(freq);
+// Initialize servos with their limits, should be called from the interface
+void Arm::initServos() {
+    initServo(10, 700, 2900, SERVO_TYPE_SC); // Shoulder1
+    initServo(11, 300, 2300, SERVO_TYPE_SC); // Shoulder2
+    initServo(0, 310, 520, SERVO_TYPE_PWM); // UpperArm
+    initServo(2, 210, 580, SERVO_TYPE_PWM); // Elbow
+    initServo(4, 110, 500, SERVO_TYPE_PWM); // ForeArm
+
+    setStartPWM(10, 2000);
+    setStartPWM(11, 2300);
+    setStartPWM(0, 510);
+    setStartPWM(2, 160);
+    setStartPWM(4, 100);
 }
 
-void Arm::setAnalogServoPWM(uint8_t servo, uint16_t off, uint16_t on) {
-    servoPWM.setPWM(servo, off, on);
+void Arm::initServo(int servo, int minPWM, int maxPWM, int type) {
+    calibrate(servo, minPWM, maxPWM);
+    servos[servo].type = type;
+
+    if (type == SERVO_TYPE_SC) {
+        scServo.EnableTorque(servo, 1);
+    }
 }
 
-void Arm::setScServoPWM(uint8_t servo, uint16_t pwm) {
-    scServo.WritePos(servo, pwm, 0, 5000);
+void Arm::setStartPWM(int servo, int pwm) {
+    servos[servo].previousPWM = pwm;
+    servos[servo].currentPWM = pwm;
+    servos[servo].nextPWM = pwm;
+}
+
+void Arm::calibrate(uint8_t servo, uint16_t min, uint16_t max) {
+    servos[servo].minPWM = min;
+    servos[servo].maxPWM = max;
+}
+
+void Arm::move(int servo, int pwm) {
+    servos[servo].currentPWMStep = 0;
+    
+    servos[servo].previousPWM = servos[servo].currentPWM;
+    servos[servo].nextPWM = pwm;
+}
+
+void Arm::updateServo(int servo) {
+    if (servos[servo].type == SERVO_TYPE_PWM) {
+        servoPWM.setPWM(servo, 0, servos[servo].currentPWM);
+    }
+    else if (servos[servo].type == SERVO_TYPE_SC) {
+        scServo.WritePos(servo, servos[servo].currentPWM, 0, 5000);
+    }
+}
+
+void Arm::update() {
+    if(updateTimer.repeat()){
+        for(int servo=0;servo < MAX_SERVO_COUNT;servo++) {
+            if (servos[servo].nextPWM != servos[servo].previousPWM) {
+                servos[servo].currentPWM = Easing::easeInOutQuad(servos[servo].currentPWMStep, servos[servo].previousPWM, servos[servo].nextPWM-servos[servo].previousPWM, easingDuration);
+
+                updateServo(servo);
+
+                // Serial.printf("%i,%i,%i,%i\n", servos[servo].currentPWM, servos[servo].nextPWM, servos[servo].previousPWM, servos[servo].currentPWMStep); // DEBUG
+
+                servos[servo].currentPWMStep++;
+
+                if (servos[servo].currentPWM == servos[servo].nextPWM) {
+                    servos[servo].previousPWM = servos[servo].nextPWM;
+                    servos[servo].currentPWMStep = 0;
+                }
+            }
+        }
+    }
 }
--- Firmware/src/Arm.h
+++ Firmware/src/Arm.h
@@ -5,19 +5,61 @@
 #include <Adafruit_PWMServoDriver.h>
 #include <SCServo.h>
 
+#include <Easing.h>
+#include <neotimer.h>
+
 
 class Arm {
     public:
         void begin();
         void beginSCServo(HardwareSerial &scServoSerial);
 
-        // Analog PWM servos
-        void setAnalogServoPWMFreq(float freq);
-        void setAnalogServoPWM(uint8_t servo, uint16_t off, uint16_t on);
+        void initServo(int servoID, int minPWM, int maxPWM, int type);
+        void initServos();
 
-        void setScServoPWM(uint8_t servo, uint16_t pwm);
+        void setStartPWM(int servo, int pwm);
+
+        // Analog PWM servos
+        void calibrate(uint8_t servo, uint16_t min, uint16_t max);
+
+        void move(int servo, int pwm);
+
+        void update();
+
+
+        uint8_t getCalibrateMin(uint8_t servo) { return servos[servo].minPWM; };
+        uint8_t getCalibrateMax(uint8_t servo) { return servos[servo].maxPWM; };
+        uint8_t getPWM(uint8_t servo) { return servos[servo].currentPWM; };
 
     private:
+        #define MAX_SERVO_COUNT 20
+
+        enum ServoType {
+            SERVO_TYPE_SC,
+            SERVO_TYPE_PWM
+        };
+
+        struct Servo {
+            int minPWM;
+            int maxPWM;
+
+            int previousPWM;
+            int currentPWM;
+            int nextPWM;
+
+            int currentPWMStep;
+
+            int easingDuration;
+
+            int type;
+
+            bool isMoving;
+        };
+
+        Servo servos[MAX_SERVO_COUNT];
+
+        void updateServo(int servo);
+
         // PWM Servos
         Adafruit_PWMServoDriver servoPWM = Adafruit_PWMServoDriver(0x40);
 
@@ -26,13 +68,9 @@ class Arm {
         uint8_t scServoTXEnablePin = 34;
         uint8_t scServoRXEnablePin = 35;
 
-        
-        uint8_t scServoID1 = 0xb;
-        uint8_t scServoID2 = 0xa;
-        /*
-        uint16_t scServoMin = 0;
-        uint16_t scServoMax = 4096;
-        */
+        int easingDuration = 200;
+
+        Neotimer updateTimer = Neotimer(15);
 };
 
 #endif
